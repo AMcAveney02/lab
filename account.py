92b5552e6351d95dab403d72725928bb45210cb7
Aaron McAveney <amcaveney@unomaha.edu>
2022-11-18 13:40:18 -0600

    Added second file and created test_account

--- account.py
+++ account.py
@@ -1,25 +1,57 @@
 class Account:
 
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
+
+        '''
+        Sets the default state of an account's name & balance.
+        :param name: Account's name
+        '''
+
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+
+        '''
+        This allows the user to deposit money into an account.
+        :param amount: The amount sent to desposit into account.
+        :return: True only if the deposit can occur, and False if not.
+        '''
+
         if amount > 0:
             self.__account_balance += amount
             return True
         elif amount <= 0:
             return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+
+        '''
+        This allows the user to withdraw money from an account.
+        :param amount: The amount sent to withdraw from an account.
+        :return: True only if the withdrawl can occur, and False if not.
+        '''
+
         if amount <= 0 or amount > self.__account_balance:
             return False
         else:
             self.__account_balance -= amount
             return True
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+
+        '''
+        This retrieves the account balance for the user.
+        :return: The account balance.
+        '''
+
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+
+        '''
+        This retreives the name of the account for the user.
+        :return: The name of the account.
+        '''
+        
         return self.__account_name
---
+++ test_account.py
@@ -0,0 +1,35 @@
+import pytest
+from account import *
+
+class Test:
+    def setUp(self):
+        self.first_account = Account("001 - Henry Cavill")
+
+    def tearDown(self):
+        del self.first_account
+
+    def test_init(self):
+        assert self.first_account.get_name() == "001 - Henry Cavill"
+        assert self.first_account.get_balance() == 0
+
+    def test_deposit(self):
+        assert self.first_account.deposit(40000) is True
+        assert self.first_account.get_balance() == 40000
+        assert self.first_account.deposit(-10000) is False
+        assert self.first_account.get_balance() == 40000
+        assert self.first_account.deposit(0) is False
+        assert self.first_account.get_balance() == 40000
+        assert self.first_account.deposit(200.7) is True
+        assert self.first_account.get_balance() == pytest.approx(40200.7, abs = 0.001)
+
+    def test_withdraw(self):
+        assert self.first_account.withdraw(40000) is False
+        assert self.first_account.get_balance() == 0
+        assert self.first_account.withdraw(-10000) is False
+        assert self.first_account.get_balance() == 0
+        assert self.first_account.withdraw(0) is False
+        assert self.first_account.get_balance() == 0
+
+        self.first_account.deposit(25000)
+        assert self.first_account.withdraw(200.5) is True
+        assert self.first_account.get_balance() == pytest.approx(24799.5, abs = 0.001)
